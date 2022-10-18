8f74b6ca8f9623570b2d780de588294c8b7e6d5e
sara <sara.camassa@heig-vd.ch>
2022-10-18 15:40:25 +0200

    first loops

--- main.cpp
+++ main.cpp
@@ -3,8 +3,37 @@
 using namespace std;
 
 int main(){
+    char quadrillage[6][7];
 
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            quadrillage[i][j] = ' ';
+            cout << quadrillage[i][j] << "|";
+        }
+        cout << endl;
+        //cout << " _ _ _ _ _ _ _" << endl;
+    }
 
+    int colonneChoisie;
+    cout << "choisissez une colonne: ";
+    cin >> colonneChoisie;
+    // si pas chiffre valide, redemande
+
+    for (int i = 5; i >= 0; i--) {
+        if(quadrillage[i][colonneChoisie] == ' '){
+            quadrillage[i][colonneChoisie] = 'o';
+            break;
+        }
+    }
+
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            //quadrillage[i][j] = ' ';
+            cout << quadrillage[i][j] << "|";
+        }
+        cout << endl;
+        //cout << " _ _ _ _ _ _ _" << endl;
+    }
 
 
     return 0;
