36593e430b5c2b5628a4cf6ea7445cec87a27c85
alans <alan.sottile@heig-vd.ch>
2022-10-18 15:55:53 +0200

    Tableau + saisie

--- main.cpp
+++ main.cpp
@@ -14,27 +14,32 @@ int main(){
         //cout << " _ _ _ _ _ _ _" << endl;
     }
 
+    while(true){
     int colonneChoisie;
-    cout << "choisissez une colonne: ";
-    cin >> colonneChoisie;
-    // si pas chiffre valide, redemande
+    bool colonnePleine = true;
 
-    for (int i = 5; i >= 0; i--) {
-        if(quadrillage[i][colonneChoisie] == ' '){
-            quadrillage[i][colonneChoisie] = 'o';
-            break;
+    while(colonnePleine == true){
+        cout << "choisissez une colonne valide: ";
+        cin >> colonneChoisie;
+        // si pas chiffre valide, redemande
+
+        for (int i = 5; i >= 0; i--) {
+            if(quadrillage[i][colonneChoisie] == ' '){
+                quadrillage[i][colonneChoisie] = 'o';
+                colonnePleine = false;
+                break;
+            }
         }
     }
 
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 7; ++j) {
-            //quadrillage[i][j] = ' ';
-            cout << quadrillage[i][j] << "|";
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                // quadrillage[i][j] = ' ';
+                cout << quadrillage[i][j] << "|";
+            }
+            cout << endl;
+            //cout << " _ _ _ _ _ _ _" << endl;
         }
-        cout << endl;
-        //cout << " _ _ _ _ _ _ _" << endl;
     }
-
-
     return 0;
 }
